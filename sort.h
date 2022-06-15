2171a1875a119123e1a302862e8d4e2de4aba147
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-06-14 23:11:32 -0500

    header update..

--- sort.h
+++ sort.h
@@ -1,16 +1,20 @@
 #ifndef SORT_H
 #define SORT_H
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
+
+
+
+/* structs */
 /**
-* struct listint_s - Doubly linked list node
-* @n: Integer stored in the node
-* @prev: Pointer to the previous element of the list
-* @next: Pointer to the next element of the list
-**/
-
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
 	const int n;
@@ -18,20 +22,25 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+#define true (1)
+#define false  (!true)
+#include "macro_functions.h"
+
+
+/* prototypes */
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
-void quick_sort(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-void swap(int *a, int *b);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
-void actual_merge(int *array, int *temp, size_t left, size_t right);
-void sub_sort(int *array, int *temp, size_t left, size_t mid, size_t right);
 void heap_sort(int *array, size_t size);
-void heapify(int *arr, int n, int i, int size);
 void radix_sort(int *array, size_t size);
-int get_max(int *array, int n);
-void count(int *array, size_t size, int *buff, int lsd);
-void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
 #endif
